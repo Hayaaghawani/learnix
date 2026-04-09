3a971c73e0c77db3948caf473c1e9dc479ab5792
halaalsallal <hal20220461@std.psut.edu.jo>
2026-04-09 03:19:43 +0300

    generalizingtheme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        luxury: ['"Cormorant Garamond"', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
